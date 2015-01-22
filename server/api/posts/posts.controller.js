b4cd66710255f083571664f5397a05da4ce5face
jnakano <jnakano89@gmail.com>
2015-01-21 16:14:27 -0800

    Comments

--- server/api/posts/posts.controller.js
+++ server/api/posts/posts.controller.js
@@ -2,13 +2,13 @@
 var Post = require('./posts.model');
 
 exports.index = function(req,res){
-
+//
 }
 
 exports.create = function(req,res){
-
+//Get parameter from url and create new model for the post 
 }
 
 exports.update = function(req,res){
-
+//Find by obect ID, and replace. 
 }
